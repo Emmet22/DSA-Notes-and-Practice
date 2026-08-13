34b5648739f1c43c9a09144571b69a18f40a6fcf
Emmet Casey <120560584+Emmet22@users.noreply.github.com>
2026-08-13 16:41:28 +0100

    Delete college-work/LDSA directory

--- college-work/LDSA/Queue.java
+++
@@ -1,8 +0,0 @@
-package assignment1;
-
-public interface Queue<T> {
-    void enqueue(T x);
-    T dequeue();
-    T next();
-    boolean empty();
-}
--- college-work/LDSA/Stack.java
+++
@@ -1,8 +0,0 @@
-package assignment1;
-
-public interface Stack<T> {
-    void push(T x);
-    T pop();
-    T peek();
-    boolean empty();
-}
